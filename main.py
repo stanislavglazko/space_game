564f77306a7b9fe73f6a0156455d9ed587b359d8
stanislavglazko <stanislav.glazko@dualbootpartners.com>
2022-11-13 18:15:22 +0100

    add custom async sleep

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ FRAMES_FOR_GARBAGE = ['trash_large.txt', 'trash_small.txt', 'trash_xl.txt']
 SHIP_FRAMES = ['rocket_frame_1.txt', 'rocket_frame_2.txt']
 
 
+async def sleep(tics=1):
+    for _ in range(tics):
+        await asyncio.sleep(0)
+
+
 async def animate_spaceship(canvas, row, column, ships_height, ships_width, height, width, ship_frame_1, ship_frame_2):
     ships = [ship_frame_1, ship_frame_1, ship_frame_2, ship_frame_2]
     iterator = cycle(ships)
@@ -29,33 +34,29 @@ async def animate_spaceship(canvas, row, column, ships_height, ships_width, heig
         draw_frame(canvas, row, column, frame)
         current_frame = frame
         time.sleep(0)
-        await asyncio.sleep(0)
+        await sleep()
 
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for i in range(random.randint(1, 20)):
-            await asyncio.sleep(0)
+        await sleep(random.randint(1, 20))
 
         canvas.addstr(row, column, symbol)
-        for i in range(random.randint(1, 3)):
-            await asyncio.sleep(0)
+        await sleep(random.randint(1, 3))
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for i in range(random.randint(1, 5)):
-            await asyncio.sleep(0)
+        await sleep(random.randint(1, 5))
 
         canvas.addstr(row, column, symbol)
-        for i in range(random.randint(1, 3)):
-            await asyncio.sleep(0)
+        await sleep(random.randint(1, 3))
 
 
 def make_stars(canvas, height, width):
     number_of_stars = random.randint(25, 100)
     stars = '+*.:'
     coroutines = []
-    for i in range(number_of_stars):
+    for _ in range(number_of_stars):
         row = random.randint(2, height - 2)
         column = random.randint(2, width - 2)
         star = random.choice(stars)
@@ -68,7 +69,7 @@ async def fill_orbit_with_garbage(coroutines, canvas, width):
     garbage_frames = make_garbage_frames()
     while True:
         coroutines.append(fly_garbage(canvas, random.randint(0, width), random.choice(garbage_frames)))
-        await asyncio.sleep(0)
+        await sleep()
 
 
 def make_garbage_frames():
@@ -87,9 +88,9 @@ def make_shot(canvas, height, width):
 
 
 def make_ship(canvas, height, width):
-    with open(f'{PATH_TO_ANIMATION}rocket_frame_1.txt', "r") as frame1:
+    with open(f'{PATH_TO_ANIMATION}{SHIP_FRAMES[0]}', 'r') as frame1:
         ship_frame_1 = frame1.read()
-    with open(f'{PATH_TO_ANIMATION}rocket_frame_2.txt', "r") as frame2:
+    with open(f'{PATH_TO_ANIMATION}{SHIP_FRAMES[1]}', 'r') as frame2:
         ship_frame_2 = frame2.read()
     ships_height, ships_width = get_frame_size(ship_frame_1)
     start_row = (height // 2) - (ships_height // 2)
