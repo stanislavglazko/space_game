c9a4e9c9341909327a60e6191d7cc1d7641f2458
stanislavglazko <stanislav.glazko@dualbootpartners.com>
2022-11-14 17:09:36 +0100

    add physic

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import time
 
 from itertools import cycle
 
+from physics import update_speed
 from space_garbage import fly_garbage
 from tools import read_controls, draw_frame, fire, get_frame_size
 
 PATH_TO_ANIMATION = './animations/'
 FRAMES_FOR_GARBAGE = ['trash_large.txt', 'trash_small.txt', 'trash_xl.txt']
 SHIP_FRAMES = ['rocket_frame_1.txt', 'rocket_frame_2.txt']
+STARS = '+*.:'
 
 
 async def sleep(tics=1):
@@ -20,17 +22,17 @@ async def sleep(tics=1):
 
 async def animate_spaceship(canvas, row, column, ships_height, ships_width, height, width, ship_frame_1, ship_frame_2):
     ships = [ship_frame_1, ship_frame_1, ship_frame_2, ship_frame_2]
+    row_speed = column_speed = 0
     iterator = cycle(ships)
     current_frame = next(iterator)
     for frame in iterator:
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
         draw_frame(canvas, row, column, current_frame, negative=True)
-        if (row + rows_direction) > 0:
-            if (row + rows_direction + ships_height) < height:
-                row += rows_direction
-        if (column + columns_direction) > 0:
-            if (column + columns_direction + ships_width) < width:
-                column += columns_direction
+        row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction)
+        if row + row_speed > 0 and row + row_speed + ships_height < height:
+            row += row_speed
+        if column + column_speed > 0 and column + column_speed + ships_width < width:
+            column += column_speed
         draw_frame(canvas, row, column, frame)
         current_frame = frame
         time.sleep(0)
@@ -54,12 +56,11 @@ async def blink(canvas, row, column, symbol='*'):
 
 def make_stars(canvas, height, width):
     number_of_stars = random.randint(25, 100)
-    stars = '+*.:'
     coroutines = []
     for _ in range(number_of_stars):
         row = random.randint(2, height - 2)
         column = random.randint(2, width - 2)
-        star = random.choice(stars)
+        star = random.choice(STARS)
         coroutine = blink(canvas, row, column, symbol=star)
         coroutines.append(coroutine)
     return coroutines
@@ -89,10 +90,10 @@ def make_shot(canvas, height, width):
 
 def make_ship(canvas, height, width):
     with open(f'{PATH_TO_ANIMATION}{SHIP_FRAMES[0]}', 'r') as frame1:
-        ship_frame_1 = frame1.read()
+        ship_frame1 = frame1.read()
     with open(f'{PATH_TO_ANIMATION}{SHIP_FRAMES[1]}', 'r') as frame2:
-        ship_frame_2 = frame2.read()
-    ships_height, ships_width = get_frame_size(ship_frame_1)
+        ship_frame2 = frame2.read()
+    ships_height, ships_width = get_frame_size(ship_frame1)
     start_row = (height // 2) - (ships_height // 2)
     start_column = (width // 2) - (ships_width // 2)
     spaceship = animate_spaceship(canvas,
@@ -102,8 +103,8 @@ def make_ship(canvas, height, width):
                                   ships_width,
                                   height,
                                   width,
-                                  ship_frame_1,
-                                  ship_frame_2,
+                                  ship_frame1,
+                                  ship_frame2,
                                   )
     return spaceship
 
