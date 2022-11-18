9bc59fd77d3df1de7ca4083c42962437fb1d88c0
stanislavglazko <stanislav.glazko@dualbootpartners.com>
2022-11-18 15:06:25 +0100

    add all garbage animations

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 PATH_TO_ANIMATIONS = './animations/'
-GARBAGE_FRAMES = ['trash_large.txt', 'trash_small.txt', 'trash_xl.txt']
+GARBAGE_FRAMES = ['trash_large.txt', 'trash_small.txt', 'trash_xl.txt', 'duck.txt', 'hubble.txt', 'lamp.txt']
 SHIP_FRAMES = ['rocket_frame_1.txt', 'rocket_frame_2.txt']
 STAR_FRAMES = '+*.:'
 GAME_OVER_FRAME = 'game_over.txt'
